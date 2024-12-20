d6a92b6063504f0605d9856ec4e20a0c4b7bca3a
abhishek <abhishekgharami1998@gmail.com>
2024-12-20 20:47:49 +0530

    # strating again

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
-LOGOUT_REDIRECT_URL = 'logout_page'
+
