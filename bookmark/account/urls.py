7b073cedbd392193ec162394e55097972055b33b
abhishek <abhishekgharami1998@gmail.com>
2024-12-21 22:13:15 +0530

    working with template for the user auth .

--- bookmark/account/urls.py
+++ bookmark/account/urls.py
@@ -6,7 +6,14 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     # password reset and password reset done view
-    path('password-reset/',auth_views.PasswordChangeView.as_view(),name='password_change'),
-    path('password-reset/done',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password-change/done',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    # password reset and password reset done view
+    path('password-reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/complete/',auth_views.PasswordResetComplete.as_view(),
+         name='password_reset_complete'),
     path('',views.dashboard,name='dashboard'),
 ]
