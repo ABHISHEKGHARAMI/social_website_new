b815563afe31c8d59d3c2386eb9c15cf37793ada
abhishek <abhishekgharami1998@gmail.com>
2024-12-22 12:21:15 +0530

    Building the registration view for the user .

--- bookmark/account/views.py
+++ bookmark/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm , UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -43,5 +43,38 @@ def dashboard(request):
         'account/dashboard.html',
         {
             'section':'dashboard'
+        }
+    )
+    
+#  register new user 
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # create new user
+            new_user = user_form.save(commit=False)
+            
+            # set the password
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # save the user
+            new_user.save()
+            return render(
+                request,
+                'account/register_done.html',
+                {
+                    'new_user':new_user
+                }
+            )
+        
+    else:
+        user_form = UserRegistrationForm()
+        
+    return render(
+        request,
+        'account/register.html',
+        {
+            'user_form':user_form
         }
     )
