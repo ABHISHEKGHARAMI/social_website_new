3ac7c2203910593d67008bf01606b02cd5415d4d
abhishek <abhishekgharami1998@gmail.com>
2024-12-23 19:28:46 +0530

    Adding the etc/hosts/ in settings .

--- bookmark/bookmark/settings.py
+++ bookmark/bookmark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4=*=ke9$nwsrwjv*m4_06!_a5(k)32#s9e8j0k6r9w=2jjm0&(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
