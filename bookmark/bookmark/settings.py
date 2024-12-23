de2d58f5f89e622d69b363d35b54851d781c844c
abhishek <abhishekgharami1998@gmail.com>
2024-12-23 20:48:25 +0530

    Trying to add outh2.

--- bookmark/bookmark/settings.py
+++ bookmark/bookmark/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -136,4 +137,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
 ]
