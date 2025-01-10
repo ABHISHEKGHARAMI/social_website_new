69532a6b4ebe9766a9323dc321d3f8ab9c8038c7
abhishek <abhishekgharami1998@gmail.com>
2025-01-10 11:16:43 +0530

    Building the views for the list and detail for the user .

--- bookmark/account/views.py
+++ bookmark/account/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login, get_user_model
 from .forms import LoginForm , UserRegistrationForm , UserEditForm , ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -118,5 +118,35 @@ def edit(request):
         {
             'user_form':user_form,
             'profile_form':profile_form
+        }
+    )
+    
+    
+# user model
+User = get_user_model()
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request,
+        'account/user/list.html',
+        {
+            'section':'people',
+            'users' : users
+        }
+    )
+    
+    
+@login_required
+def user_detail(request,username):
+    user = get_object_or_404(User,username=username,is_active=True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {
+            'section':'people',
+            'user':user
         }
     )
