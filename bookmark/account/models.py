2a8ebd35ee8acb33d749e8c9c61efa798e2ceb34
abhishek <abhishekgharami1998@gmail.com>
2025-01-10 11:07:43 +0530

    Added the migrations for the account intermediate models.

--- bookmark/account/models.py
+++ bookmark/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -44,4 +45,17 @@ class Contact(models.Model):
     
     def __str__(self):
         return f'{self.user_from} follows {self.user_to}'
+    
+    
+#  add the user dynamically
+user_model = get_user_model()
+user_model.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='follower',
+        symmetrical=False
+    )
+)
     
