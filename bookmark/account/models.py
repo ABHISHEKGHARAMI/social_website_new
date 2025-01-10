b946205ab50d3cffaecd82c172b2974a790730b6
abhishek <abhishekgharami1998@gmail.com>
2025-01-10 10:56:58 +0530

    Adding the intermediate model for the user to follow each other .

--- bookmark/account/models.py
+++ bookmark/account/models.py
@@ -18,4 +18,30 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'profile of: {self.user.username}'
+
+
+
+# creating the user intermediate model for the user
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_set',
+        on_delete=models.CASCADE
+    )
+    
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_to_set',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    
+    # meta class
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+    
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
     
